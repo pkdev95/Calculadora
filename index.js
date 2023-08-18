9e9ed1808e95225f405dec6666616b28f898d3c9
pkdev95 <113390894+pkdev95@users.noreply.github.com>
2023-08-18 19:24:16 -0300

    Implementa um layout mais responsivo e agradável ao usuário.

--- index.js
+++ index.js
@@ -1,42 +1,41 @@
-
-
-
 const escolhaOperacao = () => {
 
-  let slct = document.getElementById('slct');
+  let slctn = document.getElementById('slct');
+  let slcd = slctn.options[slctn.selectedIndex].text;
   let operacao = document.getElementById('opr');
 
   let visibilityON = () => {
     operacao.style.visibility = "visible";
   }
 
+    switch(slcd) {
+      case "Selecione":
+        operacao.style.visibility = "hidden";
+        break;
 
-  if (slct.options[slct.selectedIndex].text === "Selecione"){
-    
-    operacao.style.visibility = "hidden";
+      case "Adição":
+        operacao.innerHTML = '<i class="fa-solid fa-plus fa-beat fa-2xl"></i>';
+        visibilityON();
+        break;
 
-  } else if (slct.options[slct.selectedIndex].text === "Adição") {
-    
-    operacao.innerHTML = '<i class="fa-solid fa-plus fa-beat fa-2xl"></i>';
-    visibilityON();
+      case "Subtração":
+      operacao.innerHTML = '<i class="fa-solid fa-minus fa-fade fa-2xl"></i>';
+      visibilityON();
+      break;
 
-  } else if (slct.options[slct.selectedIndex].text === "Subtração") {
+      case "Multiplicação":
+      operacao.innerHTML = '<i class="fa-solid fa-x fa-shake fa-2xl"></i>';
+      visibilityON();
+      break;
 
-    operacao.innerHTML =  '<i class="fa-solid fa-minus fa-fade fa-2xl"></i>';
-    visibilityON();
+      case "Divisão":
+      operacao.innerHTML = '<i class="fa-solid fa-divide fa-beat fa-2xl"></i>';
+      visibilityON();
+      break;
 
-  } else if (slct.options[slct.selectedIndex].text === "Multiplicação") {
-
-    operacao.innerHTML = '<i class="fa-solid fa-x fa-shake fa-2xl"></i>';
-    visibilityON();;
-   
-
-  } else if (slct.options[slct.selectedIndex].text === "Divisão") {
-
-    operacao.innerHTML = '<i class="fa-solid fa-divide fa-beat fa-2xl"></i>';
-    visibilityON();
-   
-  } 
+        default:
+        
+    }
 }
 
 
@@ -45,48 +44,46 @@ const calculo = () => {
   let num1 = parseInt(document.getElementById('num1').value);
   let num2 = parseInt(document.getElementById('num2').value);
   let resultado = document.getElementById('rs');
+  let slctn = document.getElementById('slct');
+  let slcd = slctn.options[slctn.selectedIndex].text;
 
   let zerarInput = () => {
     document.getElementById('num1').value = '';
     document.getElementById('num2').value = '';
   }
 
-if (slct.options[slct.selectedIndex].text === "Selecione"){
+if (slcd === "Selecione"){
  
  Swal.fire('Escolha uma operação primeiro');
     
 }else if(isNaN(num1) || isNaN(num2)){
   Swal.fire('Por favor, preencha todos os campos');
 
-}else if (slct.options[slct.selectedIndex].text === "Adição") {
+}else if (slcd === "Adição") {
 
     let soma = num1 + num2;
     resultado.innerHTML =  soma;
     zerarInput();
    
-  } else if (slct.options[slct.selectedIndex].text === "Subtração") {
+  } else if (slcd === "Subtração") {
 
     let subtracao = num1 - num2;
     resultado.innerHTML =  subtracao;
     zerarInput();
 
 
-  } else if (slct.options[slct.selectedIndex].text === "Multiplicação") {
+  } else if (slcd === "Multiplicação") {
 
     let multiplicacao = num1 * num2;
     resultado.innerHTML =  multiplicacao;
     zerarInput();
     
 
-  } else if (slct.options[slct.selectedIndex].text === "Divisão") {
+  } else if (slcd === "Divisão") {
     let divisao = num1 / num2;
     resultado.innerHTML = divisao;
     zerarInput();
-
-
   } 
 
 }
-
-
 
